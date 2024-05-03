67bad798216b34c7d87b0c03197037d730753b18
GEEKMD099 <159744285+GEEKMD099@users.noreply.github.com>
2024-05-03 04:29:17 +0100

    Update _pm2.js

--- commands/_pm2.js
+++ commands/_pm2.js
@@ -3,9 +3,9 @@
  Licensed under the  GPL-3.0 License;
  You may not use this file except in compliance with the License.
  It is supplied in the hope that it may be useful.
- * @project_name : bat-Md
- * @author : Xcelsama 
- * @description : Bat-MD,A Multi-functional whatsapp bot.
+ * @project_name : shelby-Md
+ * @author : Shelbysama 
+ * @description : shelby-MD,A Multi-functional whatsapp bot.
  * @version 0.0.6
  **/
 
@@ -20,7 +20,7 @@ cmd({
     async(Void, citel,text,{ isCreator }) => {
    if (!isCreator) return citel.reply(tlang().owner)
             const { exec } = require("child_process")
-            citel.reply('Bat-Bot is Restarting')
+            citel.reply('Shelby-Bot is Restarting')
             exec('pm2 restart all')
     }
 );
